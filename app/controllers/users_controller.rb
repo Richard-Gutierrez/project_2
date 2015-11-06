59dd19415bd3c505602d5c8a8477c774d400222a
Richard <ricardoaguilar91@yahoo.com>
2015-11-05 21:03:25 -0800

    new pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,38 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "You have successfully signed up!"
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
 
 
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
     resources :articles
     resources :users
-    root 'welcome#index'
+    root 'users#new'
 
   end
 
