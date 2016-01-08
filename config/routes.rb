6257f28e48dc5ea134db57fffcea34e19bf7867b
Richard <ricardoaguilar91@yahoo.com>
2016-01-07 19:24:30 -0800

    fixed the first part of my MVC

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :payments
   resources :users
   resources :cards
-  root 'users#new'
   resources :orders, :collection => {:confirm => :get}
-
+  get 'welcome' => 'users#new'
 
 end
 
